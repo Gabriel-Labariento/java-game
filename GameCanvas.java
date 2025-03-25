fac2c4976dda611847ceecc5d09e0a7eb3f345c1
Gabriel Matthew Labariento <gabrielmatthew.labariento@gmail.com>
2025-03-25 09:44:25 +0800

    Starter files

---
+++ GameCanvas.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class GameCanvas extends JComponent {
+    private int width, height;
+
+    public GameCanvas(int width, int height){
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g){
+        
+    }
+
+}
---
+++ GameFrame.java
@@ -0,0 +1,5 @@
+impo
+
+public class GameFrame {
+    
+}
